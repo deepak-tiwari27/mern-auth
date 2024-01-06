9282ca41791765436d56927c1255498441ecfd41
deepak-tiwari27 <99322641+deepak-tiwari27@users.noreply.github.com>
2024-01-07 00:52:35 +0530

    add redux tool kit

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice.js";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Signin() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate()
+  const { loading, error } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -15,8 +21,7 @@ export default function Signin() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
-      setError(false);
+      dispatch(signInStart());
       const response = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -25,15 +30,16 @@ export default function Signin() {
         body: JSON.stringify(formData),
       });
       const data = await response.json();
-    
-      setLoading(false);
+      
       if (data.success === false) {
-        setError(true);
+        dispatch(signInFailure(data));
+        return
       }
-      navigate("/")
+      
+      dispatch(signInSuccess(data))
+   navigate("/");
     } catch (error) {
-      setLoading(false);
-      setError(true);
+      dispatch(signInFailure(error));
     }
   };
 
@@ -41,22 +47,21 @@ export default function Signin() {
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7"> Sign In</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-       
         <input
           type="email"
           placeholder="enter email"
           id="email"
           className="bg-slate-200 rounded-lg p-3"
-          autocomplete="off"
           onChange={handleChange}
+          autoComplete="off"
         />
         <input
           type="password"
           placeholder="enter password"
           id="password"
           className="bg-slate-200 rounded-lg p-3"
-          autocomplete="off"
           onChange={handleChange}
+          autoComplete="off"
         />
         <button
           disabled={loading}
@@ -71,7 +76,7 @@ export default function Signin() {
           <span className="text-blue-500">Sign up</span>
         </Link>
       </div>
-      {error && <p className="text-red-800">something went wrong</p>}
+     <p className = "text-red-700">{error?error.message||"something went wrong":""}</p>
     </div>
   );
 }
