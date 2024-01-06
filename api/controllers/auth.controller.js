70e49246755c090252898aeec922d19f6c69857d
deepak-tiwari27 <99322641+deepak-tiwari27@users.noreply.github.com>
2024-01-06 19:10:55 +0530

    create siugn in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,18 +1,42 @@
-import User from "../models/user.model.js"
-import bcryptjs from "bcryptjs"
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json({ mesage: "user created " });
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const signup = async (req,res,next)=>{
-const {username,email,password} = req.body
-const hashedPassword = bcryptjs.hashSync(password,10)
-const newUser = new User({username,email,password:hashedPassword})
-try {
-    await newUser.save()
-    res.status(201).json({mesage:"user created "})
-    
-} catch (error) {
-   next(error)
-    
-    
-}
-}
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(403, "wrong credentials"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    const oneWeekInSeconds = 7 * 24 * 60 * 60; // 7 days in seconds
+
+    const expiryDate = new Date();
+     expiryDate.setTime(expiryDate.getTime() + oneWeekInSeconds * 1000);
+    res
+      .cookie("access_token", token, { httpOnly: true, expires:expiryDate })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
