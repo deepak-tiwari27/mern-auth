d3a07baae58728af202d3d81f9c0873f363b3637
deepak-tiwari27 <99322641+deepak-tiwari27@users.noreply.github.com>
2024-01-08 14:21:43 +0530

    update header comp[onenet and make profile private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 export default function Header() {
+  const {currentUser} = useSelector(state=>state.user)
   return (
     <div className = "bg-slate-200">
         <div className="flex justify-between items-center max-w-6xl mx-auto
@@ -10,7 +12,7 @@ export default function Header() {
            <ul className = "flex gap-4">
            <Link to ="/"> <li>Home</li></Link>
            <Link to ="/about"> <li>About</li></Link>
-           <Link to ="/sign-in"> <li>Sign In</li></Link>
+           <Link to ="/profile"> {currentUser?<img src={currentUser.profilePicture} alt="user-photo" className ="h-7 w-7 rounded-full object-cover"/>:<li>Sign In</li>}</Link>
             
             
             </ul> 
